47ba0596ea5948c1163bb40f05e892c5a6f8a417
Venla, Nuutinen <ah3200@student.jamk.fi>
2026-04-28 09:29:23 +0300

    Kortti.svelte luotu

---
+++ JyvasPolut/src/lib/components/Card.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	interface Props {
+		title: string;
+		desc: string;
+		kuva: string[];
+	}
+
+	let { title, kuva, desc }: Props = $props();
+</script>
+
+<div>
+	<img src={kuva[0]} alt={title} />
+	<div>
+		<h2>{title}</h2>
+		<p>{desc}</p>
+	</div>
+</div>
